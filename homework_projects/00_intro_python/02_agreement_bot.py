ce8d4a71754ab59e2d0aa90eb8a8a4bafb055cd6
Hamza-Sheikh <hamzahamza.sheikh2009@gmail.com>
2025-03-18 15:23:33 +0500

    02_agreement_bot

---
+++ homework_projects/00_intro_python/02_agreement_bot.py
@@ -0,0 +1,7 @@
+def main():
+    favourite_animal = input("What is your favourite animal? ")
+    print(f"\nMy favorite animal is also {favourite_animal}!")
+
+
+if __name__ == "__main__":
+    main()
